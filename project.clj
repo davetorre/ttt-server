f8c850483857c1c7900f1edaccad3b3ee7148f08
David Torre <vidtorre@gmail.com>
2014-09-25 14:58:56 -0500

    Serve basic form

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [tic_tac_toe "0.1.0-SNAPSHOT"]]
+  :resource-paths ["resources/httpserver-1.0-SNAPSHOT.jar"]
   :main ^:skip-aot ttt-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ttt_server/core.clj
+++ src/ttt_server/core.clj
@@ -1,7 +1,51 @@
 (ns ttt-server.core
-  (:gen-class))
+  (:require [ttt-server.server-io :refer :all]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def board "Where the board goes.")
+
+(def form
+  "<form action=\"/move\" method=\"post\" >
+  <div>
+    <input name=\"the-move\">
+  </div>
+  <div class=\"button\">
+    <button type=\"submit\">Send</button>
+  </div>
+</form>")
+
+(def game-page
+  (str "<!DOCTYPE html><html><head></head><body>"
+       board
+       form
+       "</body></html"))
+
+(defn get-gamepage [request]
+  (new davetorre.httpserver.HTTPResponse
+       "HTTP/1.1 200 OK\n"
+       (new java.util.HashMap)
+       (.getBytes game-page)))
+
+(defn post-move [request]
+  (new davetorre.httpserver.HTTPResponse
+       "HTTP/1.1 200 OK\n"
+       (new java.util.HashMap)
+       (.body request)))
+
+(defn make-route-lamb [function-to-call]
+  (reify
+    davetorre.httpserver.ResponderCreator
+    (create [this]
+      (reify
+        davetorre.httpserver.Responder
+        (respond [this request]
+          (function-to-call request))))))
+
+(def router
+  (doto (new davetorre.httpserver.Router)
+    (.addRoute "GET /" (make-route-lamb get-gamepage))
+    (.addRoute "POST /move" (make-route-lamb post-move))))
+
+
+(defn -main [& args]
+  (let [server (new davetorre.httpserver.Server router)]
+    (.serve server 5000)))
